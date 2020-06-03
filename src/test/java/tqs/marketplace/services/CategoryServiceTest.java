a702352a0acefeafa8f77c1989e3eee45cea83e3
RicardoAntao <r.n.l.a@ua.pt>
2020-06-03 16:08:51 +0100

    Initial Product and Category Services tests

--- src/test/java/tqs/marketplace/services/CategoryServiceTest.java
+++ src/test/java/tqs/marketplace/services/CategoryServiceTest.java
@@ -1,5 +1,51 @@
+package tqs.marketplace.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tqs.marketplace.entities.Category;
+import tqs.marketplace.repositories.CategoryRepository;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryServiceTest {
+
+    @MockBean
+    private CategoryService service;
+
+
+    @Test
+    void CreateAndSaveCategoriesTest() {
+        String testCategory = "Mobile";
+        service.createCategory(testCategory);
+        assertEquals(testCategory, service.getCategory(testCategory).getCategoryName());
+    }
+
+    @Test
+    void SaveCategoriesTest(){
+        service.saveCategories();
+        assertEquals("Components", service.getCategory("Components").getCategoryName());
+        assertEquals("Computers", service.getCategory("Computers").getCategoryName());
+        assertEquals("Mobile Devices", service.getCategory("Mobile Devices").getCategoryName());
+    }
+
+
+    @Test
+    void getAllTest() {
+        String categoryNameTest = "TV";
+        Category categoryTest = new Category(categoryNameTest);
+        service.createCategory(categoryNameTest);
+        assertTrue(service.getAll().contains(categoryTest));
+    }
+
+    @Test
+    void AttemptToAddAnAlreadyExistingCategoryTest(){
+        fail();
+    }
+
+    @Test
+    void GetCategoryWhenNoCategoriesExistTest(){
+        fail();
+    }
 
 }
--- src/test/java/tqs/marketplace/services/ProductServiceTest.java
+++ src/test/java/tqs/marketplace/services/ProductServiceTest.java
@@ -1,5 +1,65 @@
+package tqs.marketplace.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import tqs.marketplace.entities.Product;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductServiceTest {
+
+    @MockBean
+    private ProductService service;
+
+    @Test
+    void ProductListIsEmptyAtStartTest(){
+        assertTrue(service.findAll().isEmpty());
+    }
+
+    @Test
+    void CreateAndSaveNewProductWithoutPicturePathTest(){
+        String testName = "Prototype Product";
+        String testDescription = "Prototype Product for testing";
+        double testPrice = 99.99;
+        Product testProduct = new Product(testName, testDescription, testPrice);
+        service.saveProduct(testName, testDescription, testPrice);
+        assertTrue(service.findByName(testName).contains(testProduct));
+        assertTrue(service.findAll().size() == 1);
+    }
+
+    @Test
+    void CreateAndSaveNewProductWithPicturePathTest(){
+        String testName = "Prototype Product";
+        String testDescription = "Prototype Product for testing";
+        double testPrice = 99.99;
+        String testPicPath = "https://lunawood.com/wp-content/uploads/2018/02/placeholder-image.png";
+        Product testProduct = new Product(testName, testDescription, testPrice, testPicPath);
+        service.saveProduct(testName, testDescription, testPrice, testPicPath);
+        assertTrue(service.findByName(testName).contains(testProduct));
+        assertTrue(service.findAll().size() == 1);
+    }
+
+    @Test
+    void findProductByIDTest(){
+        String testName = "Prototype Product";
+        String testDescription = "Prototype Product for testing";
+        double testPrice = 99.99;
+        String testPicPath = "https://lunawood.com/wp-content/uploads/2018/02/placeholder-image.png";
+        service.saveProduct(testName, testDescription, testPrice, testPicPath);
+        assertEquals(0, service.findById(1).getId());
+    }
+
+    @Test
+    void addingTwoEqualProductsTest(){
+        String testName = "Prototype Product";
+        String testDescription = "Prototype Product for testing";
+        double testPrice = 99.99;
+        String testPicPath = "https://lunawood.com/wp-content/uploads/2018/02/placeholder-image.png";
+        service.saveProduct(testName, testDescription, testPrice, testPicPath);
+        service.saveProduct(testName, testDescription, testPrice, testPicPath);
+        assertTrue(service.findAll().size() < 2);
+    }
+
 
 }
