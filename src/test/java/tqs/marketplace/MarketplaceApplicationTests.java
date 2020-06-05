c2769f6196847eb7babf41fc9a3fbebc58b82402
anunia <55753290+anunia@users.noreply.github.com>
2020-06-05 01:32:01 +0100

    Update MarketplaceApplicationTests.java

--- src/test/java/tqs/marketplace/MarketplaceApplicationTests.java
+++ src/test/java/tqs/marketplace/MarketplaceApplicationTests.java
@@ -2,8 +2,11 @@ package tqs.marketplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import tqs.marketplace.controllers.ProductController;
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 
 @SpringBootTest
